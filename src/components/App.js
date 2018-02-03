71dc9bd494f46b3792b8746ac5fdc3302ee05ebd
Jinto Jose <jintoppy@gmail.com>
2018-02-03 11:53:35 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Container from './Container';
+import Ribbon from './Ribbon';
+import '../style/App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Ribbon />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackConfig = require('./base');
+
+webpackConfig.module.rules.push({
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: [
+    'react-hot-loader/webpack',
+    'babel-loader'
+  ]
+});
+
+module.exports = Object.assign({}, webpackConfig, {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  output: {
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, '../', 'dist/'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    compress: false,
+    historyApiFallback: true,
+    stats: {
+      colors: true,
+      timings: true,
+      version: true,
+      warnings: true
+    }
+  }
+});
